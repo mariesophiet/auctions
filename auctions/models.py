6db2c4d80c2786bc453f14842c7340ed9f8e3fbe
Marie-Sophie Trübenbach <ufsda@student.kit.edu>
2022-08-23 16:24:05 +0200

    added database for user, listing, bidding, comments and display biddings on index with a picture

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,29 @@
+from datetime import datetime
+from email.policy import default
+from tkinter import CASCADE
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lister")
+    img = models.ImageField(upload_to="images/", default=None)
+    
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    comment = models.CharField(max_length=500)
+    date = models.DateTimeField(default=datetime.now, blank=True)
--- commerce/settings.py
+++ commerce/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# for displaying images
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR + "/media/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -120,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Automaticalyl create Primary Key in DB
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
